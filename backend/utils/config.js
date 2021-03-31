274d27242f3c4d8879b3c8c997009208f3146960
Mrugesh Master <mrugesh.master@gmail.com>
2021-03-31 00:56:48 -0700

    Deleted config

--- backend/utils/config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  secret: 'splitwisejwtsecretkey',
-  AWS_REGION: 'us-west-2',
-  AWS_SECRET_ACCESS_KEY: 'L9LSK5EQAutSZa/NQQJZwt8u0m7PTD9bQAvRCYVC',
-  AWS_ACCESS_KEY_ID: 'AKIA3BPMGYYGTVJAGSYE',
-  AWS_BUCKET_NAME: 'splitwise-imagestore',
-};
-module.exports = config;
